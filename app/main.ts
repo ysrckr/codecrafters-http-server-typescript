5c7272b9f49c5873941c90e4bb3b05860787a915
Yasar Cakir <dev.yasarcakir@gmail.com>
2025-07-31 23:54:33 +0700

    Fix HTTP response newline format in main.ts

--- app/main.ts
+++ app/main.ts
@@ -8,7 +8,7 @@ const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
-  socket.write("HTTP/1.1 200 OK\n\r\n\r")
+  socket.write("HTTP/1.1 200 OK\r\n\r\n")
 });
 
 server.listen(4221, "localhost");
